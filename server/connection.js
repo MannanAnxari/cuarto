3b1f205becdddddd7354205d3e871a969c8c99a6
Mannan <mannananxari@gmail.com>
2022-07-21 11:44:53 +0500

    msg1

---
+++ server/connection.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// mongodb+srv://Mnan:Mannan.1@cluster0.wgz54f5.mongodb.net/?retryWrites=true&w=majority/oscuro
+
+// mongoose.connect(`mongodb://127.0.0.1:27017/oscuro`, ()=> {
+//   console.log('connected to mongodb')
+// })
+
+mongoose.connect('mongodb://127.0.0.1:27017/oscuro',
+// mongoose.connect('mongodb+srv://Mnan:Mannan.1@cluster0.wgz54f5.mongodb.net/oscura?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+  }
+).then(() => {
+  console.log("DB Was Connected");
+}).catch((err) => {
+  console.log(err);
+})
